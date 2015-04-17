ae40584c46cddc560b0e41dbfdccc60b26e1a87d
“Andreamsofcake” <andrea.moulding@gmail.com>
2015-04-17 18:16:46 -0400

    mvp of blacklist function

--- twitter.js
+++ twitter.js
@@ -6,34 +6,50 @@ var client = new TwitterStreamChannels(credentials);
 
 var channels = {
  "languages" : [
-		'#AdidasRunning', 
-		'#adidasrunning', 
-		'#bostonmarathon',
-		'#BostonMarathon', 
-		'#bostonrunbase',
-		'#BostonRunbase',
-		'#BostonRunBase'
+		'#andreamsofcake',
+    '#AndreamsOfCake',
+    '#andreamsOfCake'
 		]
 };
+var blackList = [
+'beer',
+'hungry',
+'dove'
+];
+
+function isBlackListed(str, arr)
+{
+  for (var i=0; i<arr.length; i++) {
+    console.log(arr[i])
+    if (str.indexOf(arr[i]) === -1) {
+      return true;
+    }
+  }
+}
 
 var stream = client.streamChannels({track:channels});
 
 stream.on('channels/languages',function(tweet){
-  if(tweet.entities.media)
-  {
-    io.emit('languages', tweet.user.profile_image_url.toString(),
-                         tweet.user.screen_name.toString(),
-                         tweet.text,
-                         tweet.entities.media[0].media_url.toString());
-    console.log('>languages',tweet.entities.media[0].media_url.toString());
+  if(isBlackListed(tweet.text.toLowerCase(), blackList) == true) {
+    console.log("black listed tweet")
+    return;
   }
-  else{
-     io.emit('languages', tweet.user.profile_image_url.toString(),
-                         tweet.user.screen_name.toString(),
-                         tweet.text,
-                         'none');
-      console.log('>languages',tweet.text);
-
+  else {
+      if(tweet.entities.media)
+    {
+      io.emit('languages', tweet.user.profile_image_url.toString(),
+                           tweet.user.screen_name.toString(),
+                           tweet.text,
+                           tweet.entities.media[0].media_url.toString());
+      console.log('>languages',tweet.entities.media[0].media_url.toString());
+    }
+    else{
+       io.emit('languages', tweet.user.profile_image_url.toString(),
+                           tweet.user.screen_name.toString(),
+                           tweet.text,
+                           'none');
+        console.log('>languages',tweet.text);
+    }
   }
 });
 
